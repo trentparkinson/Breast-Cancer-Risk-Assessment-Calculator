0931b8afebde8a41902f9b73aec9e1ecbde8b370
parkinsontrent425 <parkinsontrent425@gmail.com>
2018-01-20 13:29:34 -0700

    First Commit

---
+++ server.R
@@ -0,0 +1,197 @@
+library(shiny)
+library(BCRA)
+library(flexdashboard)
+library(shinydashboard)
+
+char_race <- c("White","African-American","Hispanic-American","Other",
+               "White","Chinese-American","Japanese-American","Filipino-American",
+               "Hawaiian-American","Other Pacific Islander","Other Asian")
+
+deter_race <- function(x) {
+    return(char_race[as.numeric(x)])
+}
+
+
+shinyServer(
+    function(input, output) {
+        pred_five <- reactive({as.numeric(round(absolute.risk(data.frame(ID=1,
+                                                    T1=as.numeric(input$age),
+                                                    T2=as.numeric(input$age)+5,
+                                                    N_Biop=as.integer(input$biopsies),
+                                                    HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                                       as.integer(input$biopsies) == 99,
+                                                                   99,as.integer(input$hyperplasia)),
+                                                    AgeMen=as.numeric(input$menstruation),
+                                                    Age1st=as.numeric(input$first_birth),
+                                                    N_Rels=as.numeric(input$relatives),
+                                                    Race =as.integer(input$race))),1))
+        })
+        
+        pred_avg <- reactive({as.numeric(round(absolute.risk(data.frame(ID=1,
+                                                                         T1=as.numeric(input$age),
+                                                                         T2=as.numeric(input$age)+5,
+                                                                         N_Biop=as.integer(input$biopsies),
+                                                                         HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                                                            as.integer(input$biopsies) == 99,
+                                                                                        99,as.integer(input$hyperplasia)),
+                                                                         AgeMen=as.numeric(input$menstruation),
+                                                                         Age1st=as.numeric(input$first_birth),
+                                                                         N_Rels=as.numeric(input$relatives),
+                                                                         Race =as.integer(input$race)),iloop = 2),1))
+        })
+        
+        pred_lifetime <- reactive({as.numeric(round(absolute.risk(data.frame(ID=1,
+                                                                             T1=as.numeric(input$age),
+                                                                             T2=90,
+                                                                             N_Biop=as.integer(input$biopsies),
+                                                                             HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                                                                as.integer(input$biopsies) == 99,
+                                                                                            99,as.integer(input$hyperplasia)),
+                                                                             AgeMen=as.numeric(input$menstruation),
+                                                                             Age1st=as.numeric(input$first_birth),
+                                                                             N_Rels=as.numeric(input$relatives),
+                                                                             Race =as.integer(input$race))),1))
+        })
+        
+        pred_avg_life <- reactive({as.numeric(round(absolute.risk(data.frame(ID=1,
+                                                                             T1=as.numeric(input$age),
+                                                                             T2=90,
+                                                                             N_Biop=as.integer(input$biopsies),
+                                                                             HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                                                                as.integer(input$biopsies) == 99,
+                                                                                            99,as.integer(input$hyperplasia)),
+                                                                             AgeMen=as.numeric(input$menstruation),
+                                                                             Age1st=as.numeric(input$first_birth),
+                                                                             N_Rels=as.numeric(input$relatives),
+                                                                             Race =as.integer(input$race)),iloop = 2),1))
+        })
+        
+        output$lifetime_pred <- eventReactive(input$do,{
+            round(absolute.risk(data.frame(ID=1,
+                                           T1=as.numeric(input$age),
+                                           T2=90,
+                                           N_Biop=as.integer(input$biopsies),
+                                           HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                              as.integer(input$biopsies) == 99,
+                                                          99,as.integer(input$hyperplasia)),
+                                           AgeMen=as.numeric(input$menstruation),
+                                           Age1st=as.numeric(input$first_birth),
+                                           N_Rels=as.numeric(input$relatives),
+                                           Race =as.integer(input$race))),1)
+        })
+        output$`5_year_pred` <- eventReactive(input$do,{
+            round(absolute.risk(data.frame(ID=1,
+                                           T1=as.numeric(input$age),
+                                           T2=as.numeric(input$age)+5,
+                                           N_Biop=as.integer(input$biopsies),
+                                           HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                              as.integer(input$biopsies) == 99,
+                                                          99,as.integer(input$hyperplasia)),
+                                           AgeMen=as.numeric(input$menstruation),
+                                           Age1st=as.numeric(input$first_birth),
+                                           N_Rels=as.numeric(input$relatives),
+                                           Race =as.integer(input$race))),1)
+        })
+        output$avg_five <- eventReactive(input$do,{
+            round(absolute.risk(data.frame(ID=1,
+                                           T1=as.numeric(input$age),
+                                           T2=as.numeric(input$age)+5,
+                                           N_Biop=as.integer(input$biopsies),
+                                           HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                              as.integer(input$biopsies) == 99,
+                                                          99,as.integer(input$hyperplasia)),
+                                           AgeMen=as.numeric(input$menstruation),
+                                           Age1st=as.numeric(input$first_birth),
+                                           N_Rels=as.numeric(input$relatives),
+                                           Race =as.integer(input$race)), iloop = 2),1)
+        })
+        output$avg_life <- eventReactive(input$do,{
+            round(absolute.risk(data.frame(ID=1,
+                                           T1=as.numeric(input$age),
+                                           T2=90,
+                                           N_Biop=as.integer(input$biopsies),
+                                           HypPlas=ifelse(as.integer(input$biopsies) == 0 |
+                                                              as.integer(input$biopsies) == 99,
+                                                          99,as.integer(input$hyperplasia)),
+                                           AgeMen=as.numeric(input$menstruation),
+                                           Age1st=as.numeric(input$first_birth),
+                                           N_Rels=as.numeric(input$relatives),
+                                           Race =as.integer(input$race)),iloop = 2),1)
+        })
+        
+        
+        observeEvent( eventExpr = input$do , handlerExpr = {
+            output$plt1 <- flexdashboard::renderGauge({
+                gauge(pred_five(),
+                      min = 0,
+                      max = 17/10,
+                      symbol = '%',
+                      label = paste("5-Year Risk"),
+                      gaugeSectors(success = c(0,14/10),
+                                   warning = c(1.4, 1.69),
+                                   danger = c(17/10,100),
+                                   colors = c("success","warning","danger")
+                ))
+            })
+        })
+        
+        text_race <- reactive({deter_race(input$race)
+            })
+        
+        observeEvent(eventExpr = input$do, handlerExpr = { 
+            output$five_yr_text <- renderText({ 
+            paste0("Based on the information provided, the woman's estimated risk for developing invasive
+                   breast cancer over the next 5 years is ",pred_five(),"% compared to a risk of ",
+                   pred_avg(), "% for an average ", input$age, " year old ", text_race()," female from the general
+                   United States population. This calculation also means that the woman's risk of NOT
+                   getting breast cancer over the next 5 years is ", 100-pred_five(), "%.")
+            })
+        })
+        
+        observeEvent(eventExpr = input$do, handlerExpr = { 
+            output$lifetime_text <- renderText({ 
+                paste0("Based on the information provided, the woman's estimated risk for developing invasive
+                   breast cancer over her lifetime (to age 90) is ",pred_lifetime() ,"% compared to a risk of ",
+                       pred_avg_life(), "% for an average ", input$age, " year old ", text_race()," female from the general
+                   United States population. This calculation also means that the woman's risk of NOT
+                   getting breast cancer over her lifetime is ", 100-pred_lifetime(), "%.")
+            })
+        })
+        
+        observeEvent(eventExpr = input$do, handlerExpr = { 
+            output$five_yr_title <- renderText({ 
+                "5-Year Risk"
+            })
+        })
+        
+        observeEvent(eventExpr = input$do, handlerExpr = { 
+            output$lifetime_title <- renderText({ 
+                "Lifetime Risk"
+            })
+        })
+        
+        observeEvent(eventExpr = input$do, handlerExpr = { 
+            output$advice_title <- renderText({ 
+                "Advice"
+            })
+        })
+        
+        observeEvent(eventExpr = input$do, handlerExpr = { 
+            output$advice_text1 <- renderText({ 
+                "Patients who have an increased risk of developing breast cancer,
+                defined as calculated 5 year risk >1.7% and are at least 35 years
+                old, are candidates for chemoprevention (such as tamoxifen or raloxifene)."
+
+            })
+        })
+        
+        observeEvent(eventExpr = input$do, handlerExpr = { 
+            output$advice_text2 <- renderText({ 
+                "Patients with elevated breast cancer risk (>1.7%) should be referred to a
+                breast surgeon to discuss possible risk reduction interventions."
+                
+            })
+        })
+        
+    }
+)
---
+++ ui.R
@@ -0,0 +1,98 @@
+library(shiny)
+library(shinydashboard)
+library(flexdashboard)
+library(shinythemes)
+
+shinyUI(fluidPage(theme = shinytheme("superhero"),
+    headerPanel("Breast Cancer Risk Assessment"),
+    fluidRow(
+    sidebarPanel(
+        h2('Questionaire'),
+        numericInput('age',"Current Age", 35, min=35, max=85, step = 1),
+        
+        selectInput('menstruation',"Age of first menstruation",
+                    choices = list("Less than 12 years old" = 11,
+                                   "12 through 13 years old" = 12,
+                                   "Greater than 13 years old" = 14,
+                                   "Unknown" = 99), selected = 99),
+        
+        selectInput('first_birth',"Age at first birth",
+                    choices = list("No births" = 98,
+                                   "Less than 20 years old"= 19,
+                                   "20 through 24 years old" = 20,
+                                   "25 through 29 years old" = 25,
+                                   "30 years old and greater"= 30,
+                                   "Unknown" = 99), selected = 99),
+        
+        selectInput('relatives',"Number of 1st degree relatives with BrCa",
+                    choices = list("Unknown" = 99,
+                                    "0 relatives" = 0,
+                                   "1 relatives" = 1,
+                                   "2 or more relatives" = 2), selected = 99),
+        
+        selectInput('biopsies', "Number of breast biopsies",
+                    choices = list("Unknown" = 99,
+                                   "no biopsies" = 0,
+                                   "1 biopsies" = 1,
+                                   "2 or more biopsies" = 2), selected = 99),
+        
+        conditionalPanel(
+            condition = "input.biopsies == 1 || input.biopsies == 2",
+            selectInput("hyperplasia", "Did the biopsy display hyperplasia",
+                        choices = list("No" = 0,
+                                       "Yes" = 1,
+                                       "Unknown" = 99), selected = 99)),
+        
+        selectInput('race',"Race",
+                    choices = list("White" = 1,
+                                   "African-American" = 2,
+                                   "Hispanic-American" = 3,
+                                   "Other (Native American and unknown race" = 4,
+                                   "Chinese-American" = 6,
+                                   "Japanese-American" = 7,
+                                   "Filipino-American" = 8,
+                                   "Hawaiian-Amerian" = 9,
+                                   "Other Pacific Islander" = 10,
+                                   "Other Asian" = 11), selected = 1),
+        
+        actionButton("do","Calculate Risk", class = "btn-primary")
+        
+    ),
+    
+    mainPanel(
+        h3('Your Results'),
+        tags$hr(),
+        span(textOutput("five_yr_title"), style="font-size: 20px"),
+        textOutput("five_yr_text"),
+        tags$br(),
+        span(textOutput("lifetime_title"), style="font-size: 20px"),
+        textOutput("lifetime_text"),
+        tags$hr(),
+        fluidRow(
+            column(7
+                   , fluidRow(
+                       column(12, span(textOutput("advice_title"), style="font-size: 20px"), tags$hr())
+                   )
+                   , fluidRow(
+                       column(12, textOutput("advice_text1"), tags$br(),
+                              textOutput("advice_text2"))
+                   )
+            )
+            , column(5, tags$br(), tags$br(), tags$br(), gaugeOutput("plt1"))
+        )
+    )),
+    
+    fluidRow(
+        tags$hr(),
+        tags$div(class = "footer",("© 2018 - "), style = "bottom:0",
+                 tags$a(
+                     href="https://www.linkedin.com/in/trent-parkinson-920836120/",
+                     target="_blank",
+                     "Trent Parkinson"),
+                 align = "center")
+        
+    )
+    
+)
+
+)
